8e848f3c6a49907d8d52bf1a257ba7569e868906
maurinhokyt <mauro.tai@escola.pr.gov.br>
2024-10-30 07:59:09 -0300

    .

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function criaCartao (categoria, pergunta, resposta){
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
     </div>
-    <divc class="resposta-cartao">
+    <div class="resposta-cartao">
         <p>${resposta}</p>
     </div>
 </div>
