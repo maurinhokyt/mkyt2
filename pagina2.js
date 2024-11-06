59ef7b88287716d0b3cb656c9355a5211e6d752f
maurinhokyt <mauro.tai@escola.pr.gov.br>
2024-11-06 07:57:35 -0300

    pagina2.js

--- pagina2.js
+++ pagina2.js
@@ -16,7 +16,7 @@ function criaCartao (categoria, pergunta, resposta){
     container.appendChild(cartao);
 }
 function conteudo(card){
-const resposta=card.querySelector('resposta-cartao');
+const resposta=card.querySelector('.resposta-cartao');
 if(resposta.style.display==='none'){
 resposta.style.display='block';
 }
